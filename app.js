80b868e6b84275e4055b668ce79b8ec9d70b902c
Ollie <ollie@Ollies-MBP.fios-router.home>
2022-01-05 21:25:24 -0500

    updated js file

--- app.js
+++ app.js
@@ -1,28 +1,18 @@
 // create your variables
 
-const imageUrl = document.querySelector('input[name="image"]');
+const imageUrl = document.getElementById('meme-image');
 const topTextInput = document.querySelector('input[name="toptext"]');
 const bottomTextInput = document.querySelector('input[name="bottomtext"]');
 const submit = document.getElementById('submit');
-const newMeme = document.getElementById('new-meme');
+const newMeme = document.querySelector('.container');
 const form = document.getElementById('My-Form');
+const reset = document.querySelector('button')
 
-
-// function to generate meme
-
-// let image;
-
-// imageUrl.addEventListener("change", function(){
-//     const imageLink = URL.createObjectURL(imageUrl.files[0]);
-//     image = new Image(); 
-//     image.src = imageLink;
-//     image.addEventListener('load', function(){
-//         generateMeme(newMeme,image, topTextInput.value, bottomTextInput.value);
-//     });
-// });
+//FUNCTION TO GENERATE MEME
 
 form.addEventListener('submit', function(evt) {
     evt.preventDefault();
+    console.log("generate meme!");
     const memeGenerator = generateMeme(
         imageUrl.value, 
         topTextInput.value, 
@@ -31,10 +21,18 @@ form.addEventListener('submit', function(evt) {
     newMeme.appendChild(memeGenerator);
 });
 
-function generateMeme(image, toptext, btmtext){
-    const meme = document.createElement('h2');
-    meme.innerHTML = image;
-    meme.innerText = (toptext, btmtext);
+//FUNCTION TO PRINT IMAGES AND TEXT
+function generateMeme(imageValue, toptext, btmtext){
+    const meme = document.createElement('div');
+    meme.innerText = toptext + btmtext;
+    const image = document.createElement('img');
+    image.src = imageValue;
+    meme.appendChild(image)
     return meme;
-
 }
+
+//FUNCTION TO RESET MEME CONTAINER
+reset.addEventListener("click", function(event){
+   event.newMeme.remove();
+  });
+
